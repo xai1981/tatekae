0f828f9a984b29b8be091dff084e38be1a3f184d
Naoyasu MANABE <naoyes@naoyes-macbookair.local>
2017-06-04 18:08:03 +0900

    add entities

---
+++ src/app/entities/income.ts
@@ -0,0 +1,43 @@
+import {Account} from './account';
+
+export class Income {
+    constructor(
+        private id: number,
+        private amount: number,
+        private date: Date,
+        private account: Account
+    ) {
+    }
+
+    getId(): number {
+        return this.id;
+    }
+
+    setId(id: number){
+        this.id = id;
+    }
+
+    getAmount(): number {
+        return this.amount;
+    }
+
+    setAmount(amount: number){
+        this.amount = amount;
+    }
+
+    getDate(): Date {
+        return this.date;
+    }
+
+    setDate(date: Date){
+        this.date = date;
+    }
+
+    getAccount(): Account {
+        return this.account;
+    }
+
+    setAccount(account: Account) {
+        this.account = account;
+    }
+}

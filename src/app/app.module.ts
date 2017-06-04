9206405f96a1c8efa97d9edd8b4ac3ede6879729
xai1981 <yusuke.amano.81@gmail.com>
2017-06-04 09:21:50 +0000

    VS2017に倣うために app コンポーネントを移動。 account コンポーネントを追加。

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { AccountComponent } from './components/account/account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,

0b70e7a5535c61b97de318536cb1e9009e0ed251
xai1981 <yusuke.amano.81@gmail.com>
2017-06-10 13:15:15 +0900

    pass tslint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from "@angular/http";
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './components/app/app.component';
 import { AccountComponent } from './components/account/account.component';
 import { AppFormComponent } from './components/app-form/app-form.component';
 
 import { AccountService } from './services/account.service';
-//hoge
 
 @NgModule({
   declarations: [

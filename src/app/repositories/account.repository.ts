d2e5bf8ec33da703d0d58760e7a2be37c80d7f98
naoyes <naoyes+github@gmail.com>
2017-06-10 14:55:41 +0900

    fix account id.

--- src/app/repositories/account.repository.ts
+++ src/app/repositories/account.repository.ts
@@ -10,8 +10,8 @@ export class AccountRepository {
 
     getAll(): Array<Account> {
         this.accounts.push(new Account(1, '財布'));
-        this.accounts.push(new Account(1, 'VISA'));
-        this.accounts.push(new Account(1, 'JCB'));
+        this.accounts.push(new Account(2, 'VISA'));
+        this.accounts.push(new Account(3, 'JCB'));
 
         return this.accounts;
     }
